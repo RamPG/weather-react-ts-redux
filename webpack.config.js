3869021662f91208820d52dc128e9f78daf751fd
git config --global user.name <vlasovnew@yandex.ru>
2020-07-11 18:08:20 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.(ts|tsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'source-map-loader'
+            },
+            {
+                test: /\.(css)$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'}
+                ]
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        corejs: 3,
+                                        useBuiltIns: 'usage',
+                                        debug: true,
+                                        modules: false
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+
+                test: /\.(ts|tsx)?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    devServer: {
+        open: true,
+        historyApiFallback: true
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }
+};
