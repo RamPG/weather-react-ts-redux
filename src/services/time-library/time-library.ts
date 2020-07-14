56ad7a120a2a5835860b7848a30f89b439c16ea0
git config --global user.name <vlasovnew@yandex.ru>
2020-07-14 15:08:30 +0300

    added some innovations in calendar

--- src/services/time-library/time-library.ts
+++ src/services/time-library/time-library.ts
@@ -1,5 +1,14 @@
 import { DateObject, ItemObject } from '../../types';
 
+function getMonthNow(): number {
+  return new Date().getMonth();
+}
+function getYearNow(): number {
+  return new Date().getFullYear();
+}
+function getDayNow(): number {
+  return new Date().getDate();
+}
 function getDaysInMonth(current: DateObject): number {
   const { year, month } = current;
   const currentDate: number = new Date(year, month, 1).getTime();
@@ -21,24 +30,36 @@ function getCalendar(current: DateObject): Array<Array<ItemObject>> {
   let prevDaysInMonth: number = getDaysInMonth({ year, month: month - 1 });
   let currentDay: number = getSundayDate({ year, month }, prevDaysInMonth);
   let flagDayInMonth: boolean = false;
+  let className = 'btn btn-warning btn-block';
   if (currentDay === 1) {
     prevDaysInMonth = currentDaysInMonth;
     flagDayInMonth = true;
+    className = 'btn btn-success btn-block';
   }
   const calendar: Array<Array<ItemObject>> = [];
   for (let i: number = 0; i < 6; i++) {
     const newWeek: Array<ItemObject> = [];
     for (let j: number = 0; j < 7; j++) {
-      const className: string = flagDayInMonth ? 'btn btn-success btn-block' : 'btn btn-danger btn-block';
       newWeek.push({ day: currentDay, classButton: className });
       currentDay += 1;
       if (currentDay > prevDaysInMonth) {
         flagDayInMonth = !flagDayInMonth;
+        className = flagDayInMonth ? 'btn btn-success btn-block' : 'btn btn-warning btn-block';
         currentDay = 1;
         prevDaysInMonth = currentDaysInMonth;
       }
     }
     calendar.push(newWeek);
+  }
+  if (year === getYearNow() && month === getMonthNow()) {
+    for (let i: number = 0; i < 5; i++) {
+      for (let j: number = 0; j < 7; j++) {
+        if (calendar[i][j].day === getDayNow()) {
+          calendar[i][j].classButton = 'btn btn-danger btn-block';
+          break;
+        }
+      }
+    }
   }
   return calendar;
 }
@@ -59,15 +80,6 @@ function getNameMonth(month: number) : string {
   ];
   return monthsNames[month];
 }
-function getMonthNow(): number {
-  return new Date().getMonth();
-}
-function getYearNow(): number {
-  return new Date().getFullYear();
-}
-/* function getDayNow(): number {
-  return new Date().getDate();
-} */
 export {
   getCalendar, getNameMonth, getMonthNow, getYearNow,
 };
